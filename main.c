20d3be3ba2059e81caf290359e51edfbb01ae4c2
杨自超 <2391985478@qq.com>
2019-01-15 17:19:28 +0800

    senond init

--- main.c
+++ main.c
@@ -2,7 +2,39 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("xitong\n");
+
+    while(1){
+
+
+
+        printf("1---cunqian\n");
+        printf("2---quqian\n");
+        printf("3---yue\n");
+        printf("4---tuichu\n");
+
+        printf("gongneng\n");
+
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+
+        }
+        if(code == 2){
+
+            
+        }
+        if(code == 3){
+
+            
+        }
+        if(code == 4){
+
+            
+        }
+    }
 
     return 0;
 
